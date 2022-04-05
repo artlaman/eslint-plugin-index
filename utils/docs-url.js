fd7be5c4c8c0fe54d636f60c67fd3cd6e7efdd5a
artlaman <7010178+artlaman@users.noreply.github.com>
2022-04-05 20:09:36 +0300

    fix(utils): `docsUrl` file name

--- utils/docs-url.js
+++ utils/docs-url.js
@@ -1,5 +1,5 @@
 const packageJson = require("../package.json");
 
 module.exports = function docsUrl(name) {
-  return `${packageJson.repository}/blob/v${packageJson.version}/rules/${name}/{name}.md`;
+  return `${packageJson.repository}/blob/v${packageJson.version}/rules/${name}/${name}.md`;
 };
